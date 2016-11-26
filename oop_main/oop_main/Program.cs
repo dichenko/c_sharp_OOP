37c538dce37750a9f0beea84dcf9af4c4ed840a1
dichenko <m@dichenko.ru>
2016-11-26 16:31:35 +0300

    1

---
+++ oop_main/oop_main/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw(2, 4, '&');
+            Draw(6, 6, '%');
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
