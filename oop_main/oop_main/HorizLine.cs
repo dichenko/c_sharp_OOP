2f834288f1d43deb54f5c937e713f0e6590220d6
dichenko <m@dichenko.ru>
2016-11-27 10:34:57 +0300

    11

---
+++ oop_main/oop_main/HorizLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_main
+{
+    class HorizLine
+    {
+        List<Point> pList;
+        public HorizLine(int xL, int xR, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xL; x <= xR; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ oop_main/oop_main/VertLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_main
+{
+    class VertLine
+    {
+
+        List<Point> pList;
+        public VertLine(int y1, int y2, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = y1; y <= y2; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
